adc1b0b36fce9094bd4d0b39f999822a83b56421
BoboLin <bobolincowo@gmail.com>
2021-05-27 14:26:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+// mongoose connect to mongoDB
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+// connecting error
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// connecting success
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 
 // route setting
@@ -12,4 +27,3 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log(`App is running on http://localhost:3000`)
 })
-  
