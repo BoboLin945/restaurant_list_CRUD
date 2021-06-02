0e79bbd8df15fbe7c727c70261a614a4a0a24ba4
BoboLin <bobolincowo@gmail.com>
2021-06-02 11:23:32 +0800

    feat:add delete todo function

--- app.js
+++ app.js
@@ -94,6 +94,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Delete
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 // setting port
 app.listen(3000, () => {
